45f6181c086fd9d07a058622cf02bf4fafd9e2a8
AlCher38 <al.cher2023@icloud.com>
2024-01-15 15:58:29 +0300

    Add function get_executed_operations

--- src/utils.py
+++ src/utils.py
@@ -13,5 +13,16 @@ def load_json():
     return json_user_operations
 
 
-def get_last_five_operations():
-    pass
+def get_executed_operations(values):
+    """
+    Последние 5 выполненных (EXECUTED) операций выведеных на экран.
+    :return: executed_operations
+    """
+    executed_operations = []
+    for value in values:
+        if value == {}:
+            continue
+        elif value['state'] == 'EXECUTED':
+            executed_operations.append(value)
+
+    return executed_operations
