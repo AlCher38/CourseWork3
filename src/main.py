38a1f4a286ef60058932452bdaaca753ad8f3c26
AlCher38 <al.cher2023@icloud.com>
2024-01-15 21:17:30 +0300

    3 f string yo main

--- src/main.py
+++ src/main.py
@@ -19,5 +19,6 @@ card_number = mask_card_number(operations)
 amount_number = mask_amount_number(operations)
 
 for operation in range(len(operations)):
-    print(f"{dates[operation]} {operations[operation]['description']}\n"
-          f"{card_number[operation]} -> {amount_number[operation]}")
+    print(f"{dates[operation]} {operations[operation]['description']}")
+    print(f"{card_number[operation]} -> Счет {amount_number[operation]}")
+    print(f"{operations[operation]['operationAmount']['amount']} {operations[operation]['operationAmount']['currency']['name']}")
