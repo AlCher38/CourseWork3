253ba893a960d3bd9aa40bdd16b072c378ad34b0
AlCher38 <al.cher2023@icloud.com>
2024-01-15 15:51:39 +0300

    Add load_json

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,14 @@ from datetime import datetime
 
 
 def load_json():
+    """
+    Открытие файла operations.json
+    :return: json_user_operations
+    """
     with open(OPERATION, 'r', encoding="UTF-8") as file:
         json_user_operations = json.load(file)
     return json_user_operations
+
+
+def get_last_five_operations():
+    pass
