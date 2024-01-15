b0323b1502c41c058a35a7bd0725c2dca8fb3242
AlCher38 <al.cher2023@icloud.com>
2024-01-15 21:04:20 +0300

    Final

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
-from config import *
 import json
+import re
 from datetime import datetime
+from config import *
 
 
 def load_json():
@@ -46,27 +47,41 @@ def change_date(date):
     date_operations = []
     for list_data in date:
         sort_data = datetime.strptime(list_data['date'], "%Y-%m-%dT%H:%M:%S.%f")
-        format_date = f"{sort_data: %d.%m.%Y}"
+        format_date = f"{sort_data:%d.%m.%Y}"
         date_operations.append(format_date)
     return date_operations
 
 
-def mask_card_number(card_number):
+def mask_card_number(card_numbers):
     """
     скрытие номера карты
+    check_format = re.findall('....', card_number['from'])
     проверка: print(mask_card_number("Maestro 1596837868705199"))
     """
-    mask_number = "{} {}** **** {}".format(card_number[:-12], card_number[-10:-8], card_number[-4:])
-    return mask_number
+    card_number_operations = []
+    for card_number in card_numbers:
+        if card_number['description'] == "Открытие вклада":
+            card_number['from'] = f"Счёт клиента: {card_number['to'][5:]}"
+        mask_card = card_number['from'].split()
+        mask_card_copy = mask_card.copy()
+        del mask_card_copy[-1]
+        card_mask = re.findall('....', mask_card[-1])
+        number_card = card_mask[0], card_mask[1][0:2] + '**', card_mask[2].replace(card_mask[2], '****'), card_mask[3:]
+        mask_number = " ".join(number_card[3])
+        card_number_operations.append(f"{' '.join(mask_card_copy)} {' '.join(list(number_card[0:3]))} {mask_number}")
+    return card_number_operations
 
 
-def mask_amount_number(amount_number):
+def mask_amount_number(amount_numbers):
     """
     скрытие номера счета
     проверка print(mask_amount_number("Счет 35383033474447895560"))
     """
-    mask_amount = "Счёт **{}".format(amount_number[-4:])
-    return mask_amount
-
-
-
+    amount_number_operations = []
+    for amount_number in amount_numbers:
+        format_to_check = re.findall('....', amount_number['to'])
+        check_to_format = format_to_check[4:]
+        number_amount = check_to_format[0].replace(check_to_format[0], '**'), check_to_format[1]
+        amount_mask = ''.join(list(number_amount))
+        amount_number_operations.append(amount_mask)
+    return amount_number_operations
